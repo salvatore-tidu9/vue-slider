90e54b0b0a1250072147b672a3bbeee42c986e5f
Salvatore Tidu <salvatoret9@outlook.it>
2021-03-19 16:28:51 +0100

    Vue_Slider_(Struttura_Iniziale)

---
+++ js/script.js
@@ -0,0 +1,20 @@
+var app = new Vue({
+
+    el: "#root",
+
+    data: {
+
+        currentPhoto: 0,
+
+        photos: [
+
+            "img/img_1.jpg",
+
+            "img/img_2.jpg",
+
+            "img/img_3.jpg",
+
+            "img/img_4.jpg"
+        ]
+    }
+})
